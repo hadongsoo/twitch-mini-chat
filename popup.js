d840ba16eec3614b22f572478a2407f88eb74ac7
hadongsoo <keass9@gmail.com>
2019-02-20 01:01:30 +0900

    update for win

--- popup.js
+++ popup.js
@@ -3,9 +3,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     const streamerName = tab.title.split(' - ')[0];
     const streamerID = tab.url.split('/')[3];
     
-    const mywin = window.open("https://www.twitch.tv/popout/"+streamerID+"/chat?popout=","mini","width=400,height=70,left=600");
+    const mywin = window.open("https://www.twitch.tv/popout/"+streamerID+"/chat?popout=","mini","width=400,height=170,left=600");
 
-    mywin.resizeTo(400, 70);                             // Resizes the new window
+    mywin.resizeTo(400, 170);                             // Resizes the new window
     mywin.focus();                                        // Sets focus to the new window
 
 });
